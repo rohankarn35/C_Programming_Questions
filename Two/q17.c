495d6336af40b516bbedb7ea2ba960e7bbc6f9a6
rohankarn <rohankarn35@gmail.com>
2022-05-12 17:08:00 +0530

    second commit

---
+++ Two/q17.c
@@ -0,0 +1,13 @@
+//DEMONSTARTION OF POINTER
+#include<stdio.h>
+int main(void)
+{
+    //Declaring local variable
+ int a;
+ int *p;
+ //Statements
+ a = 14;
+ p = &a;
+ printf("%d %p\n",a ,&a);
+ 
+}
